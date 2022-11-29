0e70fd6be3c33f487152a57f21b6e22b1b77e316
Ruben Lopez <ruben.villota@impartner.com>
2022-11-29 17:34:31 -0500

    adding simply try catch block

--- IotChallenge/Program.cs
+++ IotChallenge/Program.cs
@@ -8,24 +8,33 @@ namespace IotChallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcomer to the income calculator");
-            Console.WriteLine("Please specify the path where the file is located");
-            var pathFile = Console.ReadLine();
-
-            if (string.IsNullOrEmpty(pathFile)) {
-                pathFile = "./Solution Items/EmployeesTestFile.txt";
-            }
-
-            string[] employees = File.ReadAllLines(pathFile);
-            if (employees.Length > 0)
+            try
             {
-                foreach (string line in employees) {
-                    Employee employee = new Employee(line);
-                    Console.WriteLine($"The amount to pay { employee.Name } is: {employee.Total} USD");
+                Console.WriteLine("Welcomer to the income calculator");
+                Console.WriteLine("Please specify the path where the file is located");
+                var pathFile = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(pathFile))
+                {
+                    pathFile = "./Solution Items/EmployeesTestFile.txt";
+                }
+
+                string[] employees = File.ReadAllLines(pathFile);
+                if (employees.Length > 0)
+                {
+                    foreach (string line in employees)
+                    {
+                        Employee employee = new Employee(line);
+                        Console.WriteLine($"The amount to pay { employee.Name } is: {employee.Total} USD");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The file does not have records to proceed. Please verify the path and try again");
                 }
             }
-            else {
-                Console.WriteLine("The file does not have records to proceed. Please verify the path and try again");
+            catch (Exception ex) {
+                Console.WriteLine($"Something went wrong. Please contact our support team with this message  {ex.Message}" );
             }
 
             Console.ReadLine();
